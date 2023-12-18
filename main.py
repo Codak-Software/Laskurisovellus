a681e2ffa8527811ca0c37265a85ab3db3ca989d
virvaldev <valtteri.virtanen.dev@gmail.com>
2023-12-18 22:24:39 +0200

    LS-3 lisätty tervehdys.

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 class Main:
     def __init__(self):
-        pass
+        self.risuaidat = "#" * 40
+        self.rivarit = "\n" * 2
+        self.tahdet = "*" * 40
+    
+        
 
     def tervehdys(self):
-        print("Toimii!")
+        print("Tervetuloa käyttämään laskurisovellusta!", end=self.rivarit)
+        print(
+            self.tahdet,
+            "     ----------------------------",
+            "  ** | (C) Codak Software, 2023 | **",
+            "     ----------------------------",
+ 
+            self.tahdet,
+       
+            sep="\n", end=self.rivarit)
+            
+        return None
+    
 
 
 def main():
     main = Main()
     main.tervehdys()
+    valikko = Valikko()
+
+def ohjelman_lopetus():
+    print("Kiitos ohjelman käytöstä!")
 
 
 if __name__ == "__main__":
     main()
 
 
-
-# Path: main.py
 
 
