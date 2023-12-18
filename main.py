b446aaeb5255fe51f4635856eddbaa9397119a8e
virvaldev <valtteri.virtanen.dev@gmail.com>
2023-12-18 19:15:52 +0200

    Alkuperäinen commit

---
+++ main.py
@@ -0,0 +1,19 @@
+class Main:
+    def __init__(self):
+        pass
+
+    def tervehdys(self):
+        print("Toimii!")
+
+
+def main():
+    main = Main()
+    main.tervehdys()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
